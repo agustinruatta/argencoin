f5e77c5d470617dd37df39af73853c024446be85
Agustín Ruatta <agustinruatta@gmail.com>
2022-11-06 16:59:38 -0300

    Test minting

--- test/Argencoin.ts
+++ test/Argencoin.ts
@@ -3,31 +3,56 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Argencoin", function () {
-  async function deployArgencoinContract() {
-    const [owner, minter] = await ethers.getSigners();
+  async function configFixtures() {
+    const [owner, minter, strange] = await ethers.getSigners();
 
     const argencoinContract = await (await ethers.getContractFactory("Argencoin")).deploy(minter.getAddress());
 
-    return { argencoinContract, owner, minter };
+    return { argencoinContract, owner, minter, strange };
+  }
+
+  function deployArgencoinContract() {
+    return loadFixture(configFixtures);
   }
 
   describe("Deployment", function () {
     it("Should deploy it", async function () {
-      const { argencoinContract } = await loadFixture(deployArgencoinContract);
+      const { argencoinContract } = await deployArgencoinContract();
 
       expect(await argencoinContract.totalSupply()).to.equal(0);
     });
 
     it("Should set the right owner", async function () {
-      const { argencoinContract, owner } = await loadFixture(deployArgencoinContract);
+      const { argencoinContract, owner } = await deployArgencoinContract();
 
       expect(await argencoinContract.hasRole(await argencoinContract.DEFAULT_ADMIN_ROLE(), owner.getAddress())).to.equal(true);
     });
 
     it("Should set the right minter", async function () {
-      const { argencoinContract, minter } = await loadFixture(deployArgencoinContract);
+      const { argencoinContract, minter } = await deployArgencoinContract();
 
       expect(await argencoinContract.hasRole(await argencoinContract.MINTER_ROLE(), minter.getAddress())).to.equal(true);
     });
+  });
+
+  describe('mint', () => {
+    it('Should raise an error if caller is not the minter', async () => {
+      const { argencoinContract, strange } = await deployArgencoinContract();
+
+      await expect(argencoinContract.connect(strange).mint(strange.getAddress(), 1)).to.be
+        .revertedWith(`AccessControl: account ${await (await strange.getAddress()).toLocaleLowerCase()} is missing role ${await argencoinContract.MINTER_ROLE()}`);
+    })
+
+    it('Should mint if caller is the minter', async () => {
+      let { argencoinContract, minter, strange } = await deployArgencoinContract();
+      argencoinContract = argencoinContract.connect(minter);
+
+      expect(await argencoinContract.totalSupply()).to.equal(0);
+      
+      argencoinContract.mint(strange.getAddress(), 100);
+
+      expect(await argencoinContract.totalSupply()).to.equal(100);
+      expect(await argencoinContract.balanceOf(strange.getAddress())).to.equal(100);
+    })
   });
 });
