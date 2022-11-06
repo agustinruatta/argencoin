4e5ca9355e837a22f79dc7e22f95b68972c441e8
Agustín Ruatta <agustinruatta@gmail.com>
2022-11-06 12:49:29 -0300

    Use role authorization system

--- contracts/Argencoin.sol
+++ contracts/Argencoin.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
 /// @custom:security-contact agustinruatta@gmail.com
-contract Argencoin is ERC20, ERC20Burnable, Ownable, ERC20FlashMint {
-    constructor() ERC20("Argencoin", "ARGC") {}
+contract Argencoin is ERC20, ERC20Burnable, AccessControl, ERC20FlashMint {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    constructor() ERC20("Argencoin", "ARGC") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
