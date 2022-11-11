de4f49db2afbbed3602661abe3c29dcc67648b9e
Agustín Ruatta <agustinruatta@gmail.com>
2022-11-11 17:42:45 -0300

    Created CentralBank

---
+++ contracts/CentralBank.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact agustinruatta@gmail.com
+/// CentralBank has the responsability to mint, burn and has the balance of users.
+contract Argencoin is Ownable {
+    constructor(address ownerAddress) {
+        
+    }
+}
---
+++ test/CentralBankTest.ts
@@ -0,0 +1,31 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('CentralBank', function () {
+  async function configFixtures() {
+    const [deployer, owner, strange] = await ethers.getSigners();
+
+    const centralBankContract = await (await ethers.getContractFactory('CentralBank')).deploy(owner.getAddress());
+
+    return { centralBankContract, deployer, owner, strange };
+  }
+
+  function deployCentralBankContract() {
+    return loadFixture(configFixtures);
+  }
+
+  describe('Deployment', function () {
+    it('Should deploy it', async () =>  {
+      const { centralBankContract } = await deployCentralBankContract();
+
+      expect(centralBankContract).to.not.null;
+    });
+
+    it('Should set the right owner', async () =>  {
+      const { centralBankContract, owner } = await deployCentralBankContract();
+
+      expect(await centralBankContract.owner()).to.equal(owner.getAddress());
+    });
+  });
+});
